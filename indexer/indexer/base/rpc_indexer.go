6cf5bd15bcfc55a09b3e0ea7c7fb2801206c03e3
sharphill <cszyf@163.com>
2025-06-16 11:43:20 +0800

    modified

--- indexer/indexer/base/rpc_indexer.go
+++ indexer/indexer/base/rpc_indexer.go
@@ -72,7 +72,7 @@ func (b *RpcIndexer) GetOrdinalsWithUtxo(utxo string) (uint64, wire.TxAssets, er
 		//err := db.GetValueFromDB(key, txn, output)
 		err := db.GetValueFromDB(key, txn, output)
 		if err != nil {
-			indexer.Log.Errorf("GetOrdinalsForUTXO %s failed, %v", utxo, err)
+			indexer.Log.Warningf("GetOrdinalsForUTXO %s failed, %v", utxo, err)
 			return err
 		}
 
@@ -110,7 +110,7 @@ func (b *RpcIndexer) GetUtxoInfo(utxo string) (*common.UtxoInfo, error) {
 		//err := db.GetValueFromDB(key, txn, output)
 		err := db.GetValueFromDB(key, txn, output)
 		if err != nil {
-			indexer.Log.Errorf("GetOrdinalsForUTXO %s failed, %v", utxo, err)
+			indexer.Log.Warningf("GetOrdinalsForUTXO %s failed, %v", utxo, err)
 			return err
 		}
 		return nil
