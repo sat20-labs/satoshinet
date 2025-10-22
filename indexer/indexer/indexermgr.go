1ef64c463f8aeb394d8bfb4e02360ff469467811
sharphill <cszyf@163.com>
2025-10-22 16:40:31 +0800

    modified

--- indexer/indexer/indexermgr.go
+++ indexer/indexer/indexermgr.go
@@ -352,9 +352,9 @@ func (p *IndexerMgr) ConnectBlock(block *wire.MsgBlock, height, tip int) {
 	// 聪网节点processBlock过程中，需要同步读取索引器数据，所以这里需要同步更新 rpcService
 	// TODO 优化indexer的设计
 	p.updateDB(height, tip)
-	//if height%1000 == 0 {  // TODO 先多检查，以后稳定了再降低检查频率
-		//p.checkSelf()
-	//}
+	if (height+1)%200 == 0 {  // TODO 先多检查，以后稳定了再降低检查频率
+		p.checkSelf()
+	}
 }
 
 func (p *IndexerMgr) DisconnectBlock(height int) {
