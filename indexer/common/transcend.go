93eb74356f056eba5d6a564b1ecc324fa32287d2
yingfeng <zhengyingfeng@gmail.com>
2026-05-08 22:13:15 +0800

    modified

--- indexer/common/transcend.go
+++ indexer/common/transcend.go
@@ -39,9 +39,11 @@ const (
 	CONTENT_TYPE_INVOKERESULT   = txscript.OP_DATA_23
 
 	// ordx
-	CONTENT_TYPE_DESTROY        = txscript.OP_DATA_40
+	CONTENT_TYPE_UNBIND         = txscript.OP_DATA_40
 	CONTENT_TYPE_SWAP           = txscript.OP_DATA_41
 	CONTENT_TYPE_BINDREFERRER   = txscript.OP_DATA_42
+	CONTENT_TYPE_FREEZE         = txscript.OP_DATA_43
+	CONTENT_TYPE_UNFREEZE       = txscript.OP_DATA_44
 
 	CONTENT_TYPE_MEMO	        = txscript.OP_DATA_75
 	CONTENT_TYPE_MAX	        = txscript.OP_DATA_75
