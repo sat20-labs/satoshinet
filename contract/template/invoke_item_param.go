c81bb1f8e140be2a1e9f3083addfb2e6d3dfc917
sharphill2022 <111875421+sharphill2022@users.noreply.github.com>
2026-07-27 23:14:00 +0800

    template: validate compact invoke parameters

--- contract/template/invoke_item_param.go
+++ contract/template/invoke_item_param.go
@@ -17,6 +17,12 @@ func decodeLimitOrderItemParam(item *InvokeItem) (LimitOrderInvokeParam, error) 
 	if err := param.Decode(item.Param); err != nil {
 		return param, fmt.Errorf("decode invoke item %d limit order param: %w", item.ID, err)
 	}
+	if err := validateInvokeItemOrderType(item, param.OrderType); err != nil {
+		return param, err
+	}
+	if err := resolveInvokeItemAssetName(item.AssetName, param.AssetName); err != nil {
+		return param, err
+	}
 	return param, nil
 }
 
@@ -44,6 +50,9 @@ func decodeRefundItemParam(item *InvokeItem) (RefundInvokeParam, error) {
 	if err := param.Decode(item.Param); err != nil {
 		return param, fmt.Errorf("decode invoke item %d refund param: %w", item.ID, err)
 	}
+	if err := validateInvokeItemOrderType(item, OrderTypeRefund); err != nil {
+		return param, err
+	}
 	return param, nil
 }
 
@@ -54,6 +63,12 @@ func decodeRemoveLiquidityItemParam(item *InvokeItem) (RemoveLiquidityInvokePara
 	}
 	if err := param.Decode(item.Param); err != nil {
 		return param, fmt.Errorf("decode invoke item %d remove liquidity param: %w", item.ID, err)
+	}
+	if err := validateInvokeItemOrderType(item, param.OrderType); err != nil {
+		return param, err
+	}
+	if err := resolveInvokeItemAssetName(item.AssetName, param.AssetName); err != nil {
+		return param, err
 	}
 	return param, nil
 }
@@ -74,6 +89,9 @@ func exchangeItemMinimumOutput(item *InvokeItem) (*scommon.Decimal, error) {
 	if err := param.Decode(item.Param); err != nil {
 		return nil, fmt.Errorf("decode invoke item %d exchange param: %w", item.ID, err)
 	}
+	if err := validateInvokeItemOrderType(item, OrderTypeExchange); err != nil {
+		return nil, err
+	}
 	return parseDecimalOrZero(param.MinOutA), nil
 }
 
@@ -85,83 +103,25 @@ func decodeAutopayConfigItemParam(item *InvokeItem) (AutopayConfigInvokeParam, e
 	if err := param.Decode(item.Param); err != nil {
 		return param, fmt.Errorf("decode invoke item %d autopay config param: %w", item.ID, err)
 	}
+	if err := validateInvokeItemOrderType(item, OrderTypeValidate); err != nil {
+		return param, err
+	}
 	return param, nil
 }
 
-func resolveInvokeItemAssetName(expected, encoded string) (string, error) {
+func resolveInvokeItemAssetName(expected, encoded string) error {
 	if expected != "" && encoded != "" && expected != encoded {
-		return "", fmt.Errorf("invoke parameter asset %q does not match contract asset %q", encoded, expected)
+		return fmt.Errorf("invoke parameter asset %q does not match contract asset %q", encoded, expected)
 	}
-	if expected != "" {
-		return expected, nil
-	}
-	return encoded, nil
+	return nil
 }
 
-func validateInvokeItemParamConsistency(item *InvokeItem) error {
-	if item == nil || len(item.Param) == 0 || item.Reason == InvokeReasonInvalid {
+func validateInvokeItemOrderType(item *InvokeItem, encoded int) error {
+	if item == nil || item.Reason == InvokeReasonInvalid {
 		return nil
 	}
-
-	checkOrderType := func(encoded int) error {
-		if item.OrderType != encoded {
-			return fmt.Errorf("invoke item %d order type %d does not match parameter order type %d", item.ID, item.OrderType, encoded)
-		}
-		return nil
+	if item.OrderType != encoded {
+		return fmt.Errorf("invoke item %d order type %d does not match parameter order type %d", item.ID, item.OrderType, encoded)
 	}
-	checkAssetName := func(encoded string) error {
-		_, err := resolveInvokeItemAssetName(item.AssetName, encoded)
-		return err
-	}
-
-	switch item.Action {
-	case InvokeAPISwap:
-		param, err := decodeLimitOrderItemParam(item)
-		if err != nil {
-			return err
-		}
-		if err := checkOrderType(param.OrderType); err != nil {
-			return err
-		}
-		return checkAssetName(param.AssetName)
-	case InvokeAPIRefund:
-		if _, err := decodeRefundItemParam(item); err != nil {
-			return err
-		}
-		return checkOrderType(OrderTypeRefund)
-	case InvokeAPIAddLiquidity:
-		var param AddLiquidityInvokeParam
-		if err := param.Decode(item.Param); err != nil {
-			return fmt.Errorf("decode invoke item %d add liquidity param: %w", item.ID, err)
-		}
-		if err := checkOrderType(param.OrderType); err != nil {
-			return err
-		}
-		return checkAssetName(param.AssetName)
-	case InvokeAPIRemoveLiquidity:
-		param, err := decodeRemoveLiquidityItemParam(item)
-		if err != nil {
-			return err
-		}
-		if err := checkOrderType(param.OrderType); err != nil {
-			return err
-		}
-		return checkAssetName(param.AssetName)
-	case InvokeAPIExchange:
-		if _, err := exchangeItemMinimumOutput(item); err != nil {
-			return err
-		}
-		return checkOrderType(OrderTypeExchange)
-	case InvokeAPIConfig:
-		if _, err := decodeAutopayConfigItemParam(item); err != nil {
-			return err
-		}
-		return checkOrderType(OrderTypeValidate)
-	case InvokeAPICancel:
-		return checkOrderType(OrderTypeCancel)
-	case InvokeAPIClose:
-		return checkOrderType(OrderTypeClose)
-	default:
-		return nil
-	}
+	return nil
 }
