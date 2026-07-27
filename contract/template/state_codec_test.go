807aee296c465cbbe5a0e36b7048d292553bd020
sharphill2022 <111875421+sharphill2022@users.noreply.github.com>
2026-07-27 23:15:37 +0800

    template: cover compact parameter consistency

--- contract/template/state_codec_test.go
+++ contract/template/state_codec_test.go
@@ -84,36 +84,28 @@ func TestInvokeItemRejectsLegacyExpandedInvokeParameters(t *testing.T) {
 	}
 }
 
-func TestInvokeItemRejectsOrderTypeParamMismatch(t *testing.T) {
-	param := mustLimitOrderItemParam(t, OrderTypeBuy, "ordx:f:test", "10", "2")
+func TestInvokeItemParameterDecoderRejectsOrderTypeMismatch(t *testing.T) {
 	item := InvokeItem{
-		ID:        7,
+		ID:        1,
 		Action:    InvokeAPISwap,
-		OrderType: OrderTypeSell,
+		OrderType: OrderTypeBuy,
 		AssetName: "ordx:f:test",
-		Param:     param,
+		Param:     mustLimitOrderItemParam(t, OrderTypeSell, "ordx:f:test", "10", "2"),
 		Reason:    InvokeReasonNormal,
 	}
-	encoded, err := json.Marshal(item)
-	require.NoError(t, err)
-
-	var decoded InvokeItem
-	err = json.Unmarshal(encoded, &decoded)
-	require.ErrorContains(t, err, "does not match parameter order type")
+	_, err := limitOrderItemPrice(&item)
+	require.ErrorContains(t, err, "order type")
 }
 
-func TestNewInvokeItemRejectsParameterAssetMismatch(t *testing.T) {
-	runtime := testLimitOrderRuntime(t)
-	contract := runtime.Address()
-	param := mustLimitOrderItemParam(t, OrderTypeBuy, "ordx:f:other", "10", "2")
-
-	_, err := NewInvokeItemFromRequest(runtime.contract, 1, ApplyInvokeRequest{
-		Action:        InvokeAPISwap,
-		Param:         param,
-		CallID:        "call",
-		FundingOutput: testContractOutput("tx", 1, contract, 30, nil),
-		Height:        10,
-		Timestamp:     20,
-	})
+func TestInvokeItemParameterDecoderRejectsAssetMismatch(t *testing.T) {
+	item := InvokeItem{
+		ID:        1,
+		Action:    InvokeAPISwap,
+		OrderType: OrderTypeBuy,
+		AssetName: "ordx:f:test",
+		Param:     mustLimitOrderItemParam(t, OrderTypeBuy, "ordx:f:other", "10", "2"),
+		Reason:    InvokeReasonNormal,
+	}
+	_, err := limitOrderItemPrice(&item)
 	require.ErrorContains(t, err, "does not match contract asset")
 }
